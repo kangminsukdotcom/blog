042e07b9a9f3e20e456adff395f75576693d55ce
kang <113992235+kangminsukdotcom@users.noreply.github.com>
2024-07-31 21:18:25 +0900

    deleted public folder

--- public/js/copy-link.js
+++
@@ -1,9 +0,0 @@
-function copyToClipboard(text) {
-    navigator.clipboard.writeText(text)
-      .then(() => {
-        alert("Copied link!: " + text);
-      })
-      .catch(err => {
-        console.error('Failed to copy link: ', err);
-      });
-  }
